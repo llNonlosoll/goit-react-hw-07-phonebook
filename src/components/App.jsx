663a1aea1cad8bc5783a4f0313dc835d3db13496
llNonlosoll <wishmasterar@gmail.com>
2023-07-09 19:32:48 +0200

    app added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading, selectContacts } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const contacts = useSelector(selectContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div
       style={{
@@ -7,10 +21,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <p>{contacts.length > 0 && JSON.stringify(contacts, null, 2)}</p>
     </div>
   );
 };
