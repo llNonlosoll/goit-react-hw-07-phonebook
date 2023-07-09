50c6e61db86d004ef3939503532abd8371a0ea6b
llNonlosoll <wishmasterar@gmail.com>
2023-07-09 19:34:52 +0200

    fixed v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading, selectContacts } from 'redux/selectors';
+import { selectContacts } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
