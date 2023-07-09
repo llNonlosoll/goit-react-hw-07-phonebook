67247d3ed402448e777c224e017b35b764b8cefb
llNonlosoll <wishmasterar@gmail.com>
2023-07-09 19:16:49 +0200

    redux added

---
+++ src/redux/operations.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://64aac2950c6d844abedeca3b.mockapi.io';
+
+// fetch all
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+// add contact
+export const addContact = createAsyncThunk(
+  'contacts/addTask',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contact);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+// delete contact
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteTask',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
